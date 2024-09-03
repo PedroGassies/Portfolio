05d1127657617e7566ac9aca8ff184d3a5c5d920
PedroGassies <148749756+PedroGassies@users.noreply.github.com>
2024-09-03 17:52:56 +0200

    add balise contact

--- portfolio/src/pages/contact.tsx
+++ portfolio/src/pages/contact.tsx
@@ -1,8 +1,9 @@
 import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
+import Balise from "../components/Balise/Balise"
 import Titre from "../components/Title/Title"
 import React from 'react';
-import  styles from "../styles/contact.module.css"
+import styles from "../styles/contact.module.css"
 import Form from '../components/Form/Form';
 
 
@@ -12,9 +13,12 @@ const ContactPage: React.FC = () => {
             <Header mainColor="#52796F"
             secondaryColor="#CAD2C5"/>
         <main className={styles.body}>
+            <div className={styles.width}>
+            <Balise className={styles.titre} textColor='#1C2226' backgroundColor='#CAD2C5' content='<contact>'></Balise>
             <div className={styles.phrases}>
             <Titre titre="Faisons quelque chose d'incroyable ensemble" />
             <p className={styles.text}>N&apos;hésitez pas à me contacter pour toute demande de renseignements, de collaboration, d&apos;infos complémentaire ou juste pour me dire bonjour par mail ici <a href="mailto:pedrogassies@gmail.com">pedrogassies@gmail.com</a>, ou via ce formulaire.</p>
+            </div>
             </div>
             <div className={styles.form}><Form /></div>
         </main>  
