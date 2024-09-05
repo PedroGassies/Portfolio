7473f5afe12518039ae77529376314f8e4f7f1f7
gassies pedro <pedrogassies@gmail.com>
2024-09-05 18:47:52 +0200

    portfolio version openclassrooms finie

--- portfolio/src/components/ProjectCard/ProjectCard.tsx
+++ portfolio/src/components/ProjectCard/ProjectCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './ProjectCard.module.css';
 import Titre from '../Title/Title';
-import Competences from "../Infos/Competences/Competences"
+import Competences from "../Infos/Competences/Competences";
 import ProjectImages from '../ProjectImages/ProjectImages'; 
 import { Project } from '../../data/projectTypes';
 
@@ -11,20 +11,34 @@ interface ProjetProps {
 
 const Projet: React.FC<ProjetProps> = ({ project }) => {
     return (
-            <div className={styles.projet}>
-                <Titre titre={project.title} />
-                <div className={styles.datas}>
+        <div className={styles.projet}>
+            <Titre titre={project.title} />
+            <div className={styles.datas}>
                 <p>{project.description}</p>
                 <div className={styles.informations}>
-                <Competences skills={project.skills}/>
-                <div className={styles.links}>
-                    <a href={project.websiteLink} target="_blank">VOIR LE SITE <picture><img src='../../../assets/greenarrow.png' alt=""></img> </picture></a>
-                    <a href={project.githubLink}>VOIR LE REPOS GITHUB <picture><img src='../../../assets/greenarrow.png' alt=""></img> </picture></a>
+                    <Competences skills={project.skills} />
+                    <div className={styles.links}>
+                        {project.websiteLink && project.websiteLink.trim() !== "" && (
+                            <a href={project.websiteLink} target="_blank" rel="noopener noreferrer">
+                                VOIR LE SITE
+                                <picture>
+                                    <img src='../../../assets/greenarrow.png' alt="" />
+                                </picture>
+                            </a>
+                        )}
+                        {project.githubLink && (
+                            <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+                                VOIR LE REPOS GITHUB
+                                <picture>
+                                    <img src='../../../assets/greenarrow.png' alt="" />
+                                </picture>
+                            </a>
+                        )}
+                    </div>
                 </div>
-                </div>
-                </div>
-                <ProjectImages images={project.images} title={project.title} />
             </div>
+            <ProjectImages images={project.images} title={project.title} />
+        </div>
     );
 };
 
